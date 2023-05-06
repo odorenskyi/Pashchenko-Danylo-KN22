13f3aefb636b0bb31d4f148a88fce8c6a99dda09
unknown <pachenkomih@gmail.com>
2023-05-06 04:10:56 +0300

    Зробив тестування модулів та звіт по лабораторній роботі

--- lab10/prj/ModulesPaschenko/main.cpp
+++ lab10/prj/ModulesPaschenko/main.cpp
@@ -19,20 +19,20 @@ float s_calculation(float x, float y, float z)
 
 void task_10_1(string inputFileName)
 {
-    ifstream f_read(inputFileName, ios::in);
+    ifstream f_read(inputFileName, ios::in);          //відкриваю файл "ВхФайл.txt" та перевіряю чи відкрився він
     if(!f_read)
     {
         cout << "Помилка відкриття файлу" << endl;
         return;
     }
 
-    string word;
+    string word;               //отримую слово з цього файлу
     getline(f_read, word);
 
     f_read.close();
 
     int countVowels = 0;
-    for(char symbol : word)
+    for(char symbol : word)   //перебираю кожний символ в слові і перевіряю голосні літери, і рахую їх
     {
         if(symbol == 'а' || symbol == 'е' || symbol == 'є' || symbol == 'и' || symbol == 'і' || symbol == 'ї' || symbol == 'о' || symbol == 'у' || symbol == 'ю' || symbol == 'я' || symbol == 'А' || symbol == 'Е' || symbol == 'Є' || symbol == 'И' || symbol == 'І' || symbol == 'Ї' || symbol == 'О' || symbol == 'У' || symbol == 'Ю' || symbol == 'Я')
         {
@@ -43,7 +43,7 @@ void task_10_1(string inputFileName)
     string droplet = "До щастя не пускає лінощів орава. У чім воно - ніхто не знає до пуття. Навчитись радісно робити кожну справу - Найперше правило щасливого життя.";
     string checkWord;
 
-    if(droplet.find(word) != std::string::npos)
+    if(droplet.find(word) != std::string::npos)   //шукаю слово з вхідного файлу в краплинці Віталія Іващенка, який зберігається в змінній droplet
     {
         checkWord = "є";
     }
@@ -52,14 +52,14 @@ void task_10_1(string inputFileName)
         checkWord = "немає";
     }
 
-    ofstream f_write("ВихФайл.txt", ios::out);
+    ofstream f_write("ВихФайл.txt", ios::out);         //відкриваю файл "ВихФайл.txt" та перевіряю чи відкрився він
     if(!f_write.is_open())
     {
         cout << "Помилка відкриття файлу" << endl;
         return;
     }
 
-    f_write << "--- Пащенко Данило ---" << endl
+    f_write << "--- Пащенко Данило ---" << endl         //записую дані в вихідний файл
     << "Центральноукраїнський Національний Технічний Університет, місто Кропивницький, Україна, 2023" << endl
     << "Кількість голосних літер в слові \"" << word << "\" з файлу \"ВхФайл.txt\": " << countVowels << endl
     << "Слова \"" << word << "\" з файлу \"ВхФайл.txt\" " << checkWord << " в краплинці Віталія Іващенка" << endl;
@@ -69,37 +69,37 @@ void task_10_1(string inputFileName)
 
 void task_10_2(string inputFileName)
 {
-    srand(time(0));
+    srand(time(0));   //функції srand, допоміжна для отримання дати дозапису в файлу
 
-    ifstream f_read(inputFileName, ios::in);
+    ifstream f_read(inputFileName, ios::in);   //відкриваю файл та перевіряю чи відкрився він
     if(!f_read)
     {
         cout << "Помилка відкриття файлу" << endl;
         return;
     }
 
-    string word;
+    string word;         //отримую слово з файлу
     getline(f_read, word);
 
     f_read.close();
-
+                                        //масив чарів який зберігае усі літери українсько алфавіту верхнього регістру
     char symbols[31] {'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ї', 'Ф', 'І', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Є', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Б', 'Ю'};
-    for(int i = 0; i < word.length(); i++)
+    for(int i = 0; i < word.length(); i++)         //двоє циклів: перший перебирає кожний символ в отриманну слові, другий перебирає кожний символ в масиві чарів symbols для кожної літери в слові word
     {
-        for(int j = 0; j < strlen(symbols); j++)
+        for(int j = 0; j < 31; j++)
         {
-            if(word[i] == symbols[j])
+            if(word[i] == symbols[j])         //порівнюється літера з кожним символом з масиву і якщо проходить перевірку, тобто літера верхнього регістру, заміняється на випадкову цифру, за допомогою функції rand
             {
                 word[i] = rand() % 10 + '0';
             }
         }
     }
-
+                                    //отримую поточний час, тобто час дозапису в файл
     time_t now = time(nullptr);
     char buffer[80];
     strftime(buffer, 80, "%H:%M:%S", localtime(&now));
 
-    ofstream f_add(inputFileName, ios::app);
+    ofstream f_add(inputFileName, ios::app);         //відкриваю цей же файл на дозапис, перевіряю чи відкрився він, і дописую результат заміни літери верхнього регістру на цифру та час дозапису в файл
     if(!f_add.is_open())
     {
         cout << "Помилка відкриття файлу" << endl;
@@ -113,16 +113,16 @@ void task_10_2(string inputFileName)
 
 void task_10_3(float x, float y, float z, int b)
 {
-    float s_calc_result = s_calculation(x, y, z);
-    auto binNumber = bitset<sizeof(int) * 8>(b);
+    float s_calc_result = s_calculation(x, y, z);   //отримую результат роботи функції s_calculation()
+    auto binNumber = bitset<sizeof(int) * 8>(b);   //перетворюю ціле число b з десятичної системи числення в двійкову систему числення
 
-    ofstream f_add("ВихФайл.txt", ios::app);
+    ofstream f_add("ВихФайл.txt", ios::app);      //відкриваю файл на дозапис та перевіряю чи відкрився він
     if(!f_add.is_open())
     {
         cout << "Помилка відкриття файлу" << endl;
         return;
     }
-
+                                            //записую результат функції s_calculation() та, перетворенне в двійкову систему числення, число b
     f_add << endl << endl << "Результат роботи функції s_calculation(): " << s_calc_result << endl
     << "Число в двійкову коді: " << binNumber;
 
--- lab10/prj/TestDriver/main.cpp
+++ lab10/prj/TestDriver/main.cpp
@@ -1,14 +1,118 @@
 #include <iostream>
 #include <ModulesPaschenko.h>
 #include <windows.h>
+#include <fstream>
 
 using namespace std;
 
-int main()
+void testDriver1()
 {
     system("chcp 1251");
     system("cls");
 
-    task_10_3(1, 1, 1, 256);
-    return 0;
+    string filesName[3] = {"¬х‘айл1.txt", "¬х‘айл2.txt", "¬х‘айл3.txt"};
+
+    for(int i = 0; i < 3; i++)
+    {
+        task_10_1(filesName[i]);
+
+        ifstream f_read("¬их‘айл.txt", ios::in);
+        if(!f_read)
+        {
+            cout << "ѕомилка в≥дкритт€ файлу" << endl;
+            return;
+        }
+
+        string result;
+        string line;
+
+        if(i == 2) break;
+
+        while(getline(f_read, line))
+        {
+            result += line + '\n';
+        }
+
+        f_read.close();
+
+        cout << "–езультат: " << endl << result << endl;
+    }
+    return;
+}
+
+void testDriver2()
+{
+    string filesName[3] = {"¬х‘айл4.txt", "¬х‘айл5.txt", "¬х‘айл6.txt"};
+
+    for(int i = 0; i < 3; i++)
+    {
+        task_10_2(filesName[i]);
+
+        if(i == 2) break;
+
+        ifstream f_read(filesName[i], ios::in);
+        if(!f_read)
+        {
+            cout << "ѕомилка в≥дкритт€ файлу" << endl;
+            return;
+        }
+
+        string result;
+        string line;
+
+        while(getline(f_read, line))
+        {
+            result += line + '\n';
+        }
+
+        f_read.close();
+        cout << "–езультат: " << endl << result << endl;
+    }
+    return;
+}
+
+void testDriver3()
+{
+    string filesName[3] = {"¬их‘айл.txt", "¬их‘айл.txt", "¬их‘айл1.txt"};
+    float numbersForS_calc[6] = {3, 1.5, 3, 3.3, 3, 15.7};
+    int numbersForBinary[2] = {33, 123};
+
+    for(int i = 0; i < 3; i++)
+    {
+
+        ifstream f_read(filesName[i], ios::in);
+        if(!f_read)
+        {
+            cout << "ѕомилка в≥дкритт€ файлу" << endl;
+            return;
+        }
+
+        if(i == 2) break;
+
+        task_10_3(numbersForS_calc[i], numbersForS_calc[i+2], numbersForS_calc[i+4], numbersForBinary[i]);
+
+
+        string result;
+        string line;
+
+        while(getline(f_read, line))
+        {
+            result += line + '\n';
+        }
+
+        f_read.close();
+
+        cout << "–езультат: " << endl << result << endl;
+    }
+    return;
+}
+
+
+
+int main()
+{
+    testDriver1();
+    testDriver2();
+    testDriver3();
+    system("pause");
 }
